5bcdd5696d687855c79859419c3b138304421cbc
cagopa <cagdas96vkp@gmail.com>
2024-06-30 22:17:06 +0300

    Added action server goal status aborted succeeded

--- src/count_until_client.cpp
+++ src/count_until_client.cpp
@@ -1,21 +1,78 @@
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "my_robot_interfaces/action/count_until.hpp"
 
+using CountUntil = my_robot_interfaces::action::CountUntil;
+using CountUntilGoalHandle = rclcpp_action::ClientGoalHandle<CountUntil>;
+using namespace std::placeholders;
 
-class MyCustomNode : public rclcpp::Node //MODIFY NAME
+class CountUntilClientNode : public rclcpp::Node 
 {
 
 public:
-    MyCustomNode() :Node("node_name"){
+    CountUntilClientNode() :Node("count_until_client")
+    {
+        count_until_client_ = rclcpp_action::create_client<CountUntil>(this, "count_until");
+    } 
 
-    } //MODIFY NAME
+    void send_goal(int target_number, double period)
+    {
+        // Wait for the action server
+        count_until_client_->wait_for_action_server();
+
+        // Create the goal
+        auto goal = CountUntil::Goal();
+        goal.target_number = target_number;
+        goal.period = period;
+
+        // Add callbacks
+        auto options = rclcpp_action::Client<CountUntil>::SendGoalOptions();
+        options.result_callback= 
+            std::bind(&CountUntilClientNode::goal_result_callback,this, _1);
+        options.goal_response_callback = 
+            std::bind(&CountUntilClientNode::goal_response_callback,this, _1);
+
+        // Send the goal
+        RCLCPP_INFO(this->get_logger(), "Sending a goal");
+        count_until_client_->async_send_goal(goal, options);
+    }
 private:
-    /* data */
-};
+    // Callback to know if the goal was accepted or rejected
+    void goal_response_callback(const CountUntilGoalHandle::SharedPtr &goal_handle)
+    {
+        if (!goal_handle)
+        {
+            RCLCPP_INFO(this->get_logger(), "Goal got rejected"); 
+        }else
+        {
+            RCLCPP_INFO(this->get_logger(), "Goal got accepted");
+        }
+    }
+
+    // Callback to receive the result once the goal is done
+    void goal_result_callback(const CountUntilGoalHandle::WrappedResult &result)
+    {
+        auto status =result.code;
+        if(status == rclcpp_action::ResultCode::SUCCEEDED)
+        {
+            RCLCPP_INFO(this->get_logger(), "Succeeded"); 
+        }else if(status == rclcpp_action::ResultCode::ABORTED)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Aborted"); 
+        }
+        int reached_number = result.result->reached_number;
+        RCLCPP_INFO(this->get_logger(),"Result: %d",reached_number);
+    }
+
+    rclcpp_action::Client<CountUntil>::SharedPtr count_until_client_;
+    
+    };
 
 
 int main(int argc,char **argv){
     rclcpp::init(argc,argv);
-    auto node = std::make_shared<MyCustomNode>(); //MODIFY NAME
+    auto node = std::make_shared<CountUntilClientNode>(); 
+    node->send_goal(6, 0.8);
     rclcpp::spin(node);
     rclcpp::shutdown();
 
